1879b1eb015439800387a066be3231d5d3918eb8
Jeroen <jeroen.thomas@ymail.com>
2021-12-02 14:37:42 +0100

    day2 added

--- C#/Day_2/Day2/Day2/Program.cs
+++ C#/Day_2/Day2/Day2/Program.cs
@@ -10,6 +10,51 @@ namespace Day2
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter input location: ");
+            var location = Console.ReadLine();
+
+            // Read the input 
+            var lines = System.IO.File.ReadAllLines(location);
+
+            Task(lines);
+
+            Console.ReadLine();
+        }
+
+
+        static void Task(string[] inputLines)
+        {
+            var task1Pos = new Dictionary<string, int>() { { "xPos", 0 }, { "yPos", 0 } };
+            var task2Pos = new Dictionary<string, int>() { { "xPos", 0 }, { "yPos", 0 }, { "aim", 0 } };
+
+            for (int x = 0; x < inputLines.Length; x++)
+            {
+                var splitted = inputLines[x].Split();
+                var direction = splitted[0];
+                var directionValue = int.Parse(splitted[1]);
+
+                switch (direction)
+                {
+                    case "forward":
+                        task1Pos["xPos"] += directionValue;
+                        task2Pos["xPos"] += directionValue;
+                        task2Pos["yPos"] += directionValue * task2Pos["aim"];
+                        break;
+                    case "up":
+                        task1Pos["yPos"] -= directionValue;
+                        task2Pos["aim"] -= directionValue;
+                        break;
+                    case "down":
+                        task1Pos["yPos"] += directionValue;
+                        task2Pos["aim"] += directionValue;
+                        break;
+                    default:
+                        throw new Exception("Incorrect direction value.");
+                }
+            }
+
+            Console.WriteLine("Task1: " + task1Pos["xPos"] * task1Pos["yPos"]);
+            Console.WriteLine("Task2: " + task2Pos["xPos"] * task2Pos["yPos"]);
         }
     }
 }
